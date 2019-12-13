7bf4ca5bb75c8c79b0f4e1247f548f4a73004340
司马黑 <653014496@qq.com>
2019-12-14 02:32:47 +0800

    Kotlin RSocket Server.

--- src/main/kotlin/com/example/stockservice/StockServiceApplication.kt
+++ src/main/kotlin/com/example/stockservice/StockServiceApplication.kt
@@ -3,6 +3,9 @@ package com.example.stockservice
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.http.MediaType
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.stereotype.Controller
+import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
@@ -20,11 +23,23 @@ fun main(args: Array<String>) {
 }
 
 @RestController
-class RestController {
+class RestController(val pricesService: PriceService) {
 
     @GetMapping(value = ["/stocks/{symbol}"],
             produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
-    fun prices(@PathVariable symbol: String): Flux<StockPrice> {
+    fun prices(@PathVariable symbol: String) = pricesService.generatePrices(symbol)
+}
+
+@Controller
+class RSocketController(val priceService: PriceService) {
+
+    @MessageMapping("stockPrices")
+    fun prices(symbol: String) = priceService.generatePrices(symbol)
+}
+
+@Service
+class PriceService {
+    fun generatePrices(symbol: String): Flux<StockPrice> {
         return Flux.interval(Duration.ofSeconds(1))
                 .map { StockPrice(symbol, randomStockPrice(), now()) }
     }
